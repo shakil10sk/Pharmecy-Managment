a7e647dd520c83a37cd3135b815b626ba3b318b0
Shakil Hussain <shakil550sh@gmail.com>
2021-07-01 21:01:58 +0600

    last edit

--- resources/views/frontend/dashboard/pages/newpage.blade.php
+++
@@ -1,6 +0,0 @@
-adfo ajdlfkjadkas jfl'asdasd;f kas;lf la;sfo;asdkf
-af paodj <div class="alert alert-success" role="alert">
-  <h4 class="alert-heading">fd opasdf a</h4>
-  <p></p>
-  <p class="mb-0"></p>
-</div>
