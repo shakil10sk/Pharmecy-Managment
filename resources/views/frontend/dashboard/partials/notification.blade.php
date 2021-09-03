7428bc22bdb42cfd11258943fdd67409ddf48760
Shakil Hossain <shakil550sh@gmail.com>
2021-09-04 00:29:48 +0600

    stock report generated

--- resources/views/frontend/dashboard/partials/notification.blade.php
+++ resources/views/frontend/dashboard/partials/notification.blade.php
@@ -17,62 +17,26 @@
         <li class="list-group">
            <!-- list item-->
 
-           @php
+        @php
             $stock=DB::table('medicines')->select('*')->where('qty','<',50)->get();           
             foreach ($stock as $data) {   
                 
-           echo '<a href="javascript:void(0);"  class="list-group-item">';
-            echo '<div class="media">';
+                echo '<a href="javascript:void(0);"  class="list-group-item">';
+                echo '<div class="media">';
                 echo '<div class="pull-left"><em onclick="error();" class="fa fa-user-plus fa-2x text-info"></em> </div>';
                     
-                
-                echo '<div class="media-body clearfix">';
-                                   
+                echo '<div class="media-body clearfix">';                                   
                        
-                    echo "<div class='media-heading'>".$data->medicine_name."</div>";
-                    echo "<p class='m-0'>";
-                    echo "<small>You have ".$data->qty." pcs Medicines </small></p>";
-                    echo "<br>";
-                    
+                echo "<div class='media-heading'>".$data->medicine_name."</div>";
+                echo "<p class='m-0'>";
+                echo "<small>You have ".$data->qty." pcs Medicines </small></p>";
+                echo "<br>";                    
                 
-                 echo "</div> </div> </a>";
-             
+                echo "</div> </div> </a>";             
           
-        }
-                        @endphp
-           {{-- <!-- list item-->
-            <a href="javascript:void(0);" class="list-group-item">
-              <div class="media">
-                 <div class="pull-left">
-                    <em class="fa fa-diamond fa-2x text-primary"></em>
-                 </div>
-                 <div class="media-body clearfix">
-                    <div class="media-heading">New settings</div>
-                    <p class="m-0">
-                       <small>There are new settings available</small>
-                    </p>
-                 </div>
-              </div>
-            </a>
-            <!-- list item-->
-            <a href="javascript:void(0);" class="list-group-item">
-              <div class="media">
-                 <div class="pull-left">
-                    <em class="fa fa-bell-o fa-2x text-danger"></em>
-                 </div>
-                 <div class="media-body clearfix">
-                    <div class="media-heading">Updates</div>
-                    <p class="m-0">
-                       <small>There are
-                          <span class="text-primary">2</span> new updates available</small>
-                    </p>
-                 </div>
-              </div>
-            </a>
-           <!-- last list item -->
-            <a href="javascript:void(0);" class="list-group-item">
-              <small>See all notifications</small>
-            </a> --}}
+            }
+        @endphp
+          
         </li>
     </ul>
 </li>
