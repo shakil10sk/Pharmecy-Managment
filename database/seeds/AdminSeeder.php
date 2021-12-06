2f0a2179e658d5234733c520c1f7cffb2adfa7a1
Shakil Hossain <shakiliit@innovationit.com.bd>
2021-12-06 15:26:15 +0600

    db seed done for admin

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -25,5 +25,18 @@ class AdminSeeder extends Seeder
             'photo' => '',
             'password' => Hash::make('12345678'),
         ]);
+
+        DB::table('users')->insert([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'role' => '2',
+            'phone' => '01355646244',
+            'nid_number' => '46454569640',
+            'city' => 'Rajshahi',
+            'position' => 'Sellsman',
+            'address' => 'Muhammadpur,Dhaka',
+            'photo' => '',
+            'password' => Hash::make('12345678'),
+        ]);
     }
 }
